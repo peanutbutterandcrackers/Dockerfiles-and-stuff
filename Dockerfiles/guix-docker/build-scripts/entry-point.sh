19425c941a1e1d392b965c4dff4285f6fbc578a0
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-10-09 21:40:38 +0545

    Redirect guix-daemon outputs to ~/guix_daemon_logs

--- Dockerfiles/guix-docker/build-scripts/entry-point.sh
+++ Dockerfiles/guix-docker/build-scripts/entry-point.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild &
+/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild &> ~/guix_daemon_logs &
 source /etc/profile
 exec "$@"
