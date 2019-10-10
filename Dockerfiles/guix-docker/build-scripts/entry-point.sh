ef3ace724132720011d9826b2dfe9421b0f81c7d
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-10-10 08:10:29 +0545

    Add a way to circumvent the requirement of --privileged containers
    If DISABLE_CHROOT environment variable is set, guix-daemon now runs
    with --disable-chroot (which is NOT the recommended way of running
    guix-daemon as it "would allow build processes to gain access to
    undeclared dependencies" [within the container, in this case]), thus
    removing the requirement of `--privileged` while spinning up a container.
    
    Users can pass `--env DISABLE_CHROOT=True` to make guix-daemon run
    with --disable-chroot. As long as DISABLE_CHROOT is set to something,
    - anything - guix-daemon will run with chroot disabled.

--- Dockerfiles/guix-docker/build-scripts/entry-point.sh
+++ Dockerfiles/guix-docker/build-scripts/entry-point.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild &> ~/guix_daemon_logs &
+/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon \
+                           --build-users-group=guixbuild \
+                           --cache-failures \
+                           ${DISABLE_CHROOT:+--disable-chroot} \
+                           &> ~/guix_daemon_logs &
 source /etc/profile
 exec "$@"
